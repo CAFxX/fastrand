691b4bc50f507b779c0f144aec2530e3f6f5cd6d
Carlo Alberto Ferraris <cafxx@mercari.com>
2021-01-03 15:04:54 +0900

    add comments

--- splitmix64.go
+++ splitmix64.go
@@ -2,8 +2,7 @@ package fastrand
 
 // SplitMix64 implements the Java 8 SplittableRandom generator.
 // It is not safe for concurrent use by multiple goroutines.
-// The zero value is a valid state: Seed() can be called to set
-// a custom seed.
+// The zero value is a valid state: Seed() can be called to set a custom seed.
 type SplitMix64 struct {
 	state uint64
 }
--- splitmix64_atomic.go
+++ splitmix64_atomic.go
@@ -4,14 +4,24 @@ import (
 	"sync/atomic"
 )
 
+// AtomicSplitMix64 implements the Java 8 SplittableRandom generator with atomic state updates.
+//
+// It is safe for concurrent use by multiple goroutines.
+// The zero value is a valid state: Seed() can be called to set a custom seed.
 type AtomicSplitMix64 struct {
 	SplitMix64
 }
 
+// Seed initializes the state with the provided seed.
+//
+// This function is safe for concurrent use by multiple goroutines.
 func (r *AtomicSplitMix64) Seed(s uint64) {
 	atomic.StoreUint64(&r.state, s)
 }
 
+// Uint64 returns a random uint64.
+//
+// This function is safe for concurrent use by multiple goroutines.
 func (r *AtomicSplitMix64) Uint64() uint64 {
 	z := atomic.AddUint64(&r.state, 0x9e3779b97f4a7c15)
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9
