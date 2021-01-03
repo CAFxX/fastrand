36eb07b4fb298e37ff7c8eb919fdc404c706a301
Carlo Alberto Ferraris <cafxx@mercari.com>
2021-01-03 15:29:46 +0900

    add comments

--- pcg.go
+++ pcg.go
@@ -4,6 +4,9 @@ import (
 	"math/bits"
 )
 
+// PCG implements the PCG-XSH-RR generator.
+// It is not safe for concurrent use by multiple goroutines.
+// The zero value is a valid state: Seed() can be called to set a custom seed.
 type PCG struct {
 	state uint64
 }
@@ -13,11 +16,17 @@ const (
 	pcgIncr = 1442695040888963407
 )
 
+// Seed initializes the state with the provided seed.
+//
+// This function is not safe for concurrent use by multiple goroutines.
 func (r *PCG) Seed(s uint64) {
 	r.state = s + pcgIncr
-	r.Uint32()
+	r.Uint32() // as done by the original C implementation
 }
 
+// Uint32 returns a random uint32.
+//
+// This function is not safe for concurrent use by multiple goroutines.
 func (r *PCG) Uint32() uint32 {
 	x := r.state
 	x = x ^ (x >> 18)
