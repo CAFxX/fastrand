4efd5d85713c98b17669c7b0340338152edb7f7e
Carlo Alberto Ferraris <cafxx@strayorange.com>
2021-09-17 12:04:28 +0000

    wip

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/CAFxX/fastrand
 go 1.15
 
 require (
-	github.com/CAFxX/atomics v0.0.0-20190606154853-c2751b778050
-	github.com/klauspost/cpuid/v2 v2.0.6
+	github.com/CAFxX/atomic128 v0.2.0
+	github.com/klauspost/cpuid/v2 v2.0.8
 	github.com/valyala/fastrand v1.0.0
 )
--- pcg_xsl_rr_atomic.go
+++ pcg_xsl_rr_atomic.go
@@ -1,34 +1,16 @@
 package fastrand
 
 import (
-	"runtime"
-	"sync/atomic"
-	"unsafe"
-
-	"github.com/CAFxX/atomics"
-	"github.com/klauspost/cpuid/v2"
+	"github.com/CAFxX/atomic128"
 )
 
-// AtomicPCG implements the PCG-XSH-RR generator with atomic state updates.
+// AtomicPCGXSLRR implements the PCG-XSL-RR generator with atomic state updates.
 //
 // This generator is safe for concurrent use by multiple goroutines.
 // The zero value is a valid state: Seed() can be called to set a custom seed.
+// This structure must not be copied.
 type AtomicPCGXSLRR struct {
-	state [2 + 1]uint64
-}
-
-func (r *AtomicPCGXSLRR) pstate() *[2]uint64 {
-	if uintptr(unsafe.Pointer(&r.state[0]))%16 == 0 {
-		return (*[2]uint64)(unsafe.Pointer(&r.state[0]))
-	}
-	return (*[2]uint64)(unsafe.Pointer(&r.state[1]))
-}
-
-func (r *AtomicPCGXSLRR) plock() *uint64 {
-	if uintptr(unsafe.Pointer(&r.state[0]))%16 == 0 {
-		return &r.state[2]
-	}
-	return &r.state[0]
+	state atomic128.Uint128
 }
 
 // Seed initializes the state with the provided seed.
@@ -37,55 +19,19 @@ func (r *AtomicPCGXSLRR) plock() *uint64 {
 func (r *AtomicPCGXSLRR) Seed(h, l uint64) {
 	var t PCGXSLRR
 	t.Seed(h, l)
-	nh, nl := t.state[0], t.state[1]
-	oh, ol := r.pstate()[0], r.pstate()[1]
-	for {
-		h, l := r.cas128(oh, ol, nh, nl)
-		if h == nh && l == nl {
-			return
-		}
-		oh, ol = h, l
-		cpuYield(1)
-	}
+	atomic128.StoreUint128(&r.state, t.state)
 }
 
 // Uint64 returns a random uint64.
 //
 // This function is safe for concurrent use by multiple goroutines.
 func (r *AtomicPCGXSLRR) Uint64() uint64 {
-	oh, ol := r.pstate()[0], r.pstate()[1]
 	for {
-		nh, nl, n := pcgxslrrStep(oh, ol)
-		h, l := r.cas128(oh, ol, nh, nl)
-		if h == nh && l == nl {
+		s := atomic128.LoadUint128(&r.state)
+		nh, nl, n := pcgxslrrStep(s[0], s[1])
+		if atomic128.CompareAndSwapUint128(&r.state, s, [2]uint64{nh, nl}) {
 			return n
 		}
-		oh, ol = h, l
 		cpuYield(1)
 	}
-}
-
-func (r *AtomicPCGXSLRR) cas128(oh, ol, nh, nl uint64) (h, l uint64) {
-	p := r.pstate()
-	if runtime.GOARCH == "amd64" && cpuid.CPU.Has(cpuid.CX16) {
-		return atomics.CompareAndSwap2xUint64(&p[0], oh, ol, nh, nl)
-	}
-	// fallback implementation
-	s := r.plock()
-	if !atomic.CompareAndSwapUint64(s, 0, 1) {
-		for {
-			if atomic.LoadUint64(s) == 0 && atomic.CompareAndSwapUint64(s, 0, 1) {
-				break
-			}
-			cpuYield(1)
-		}
-	}
-	ch, cl := p[0], p[1]
-	if ch != oh || cl != ol {
-		atomic.StoreUint64(s, 0)
-		return ch, cl
-	}
-	p[0], p[1] = nh, nl
-	atomic.StoreUint64(s, 0)
-	return nh, nl
 }
